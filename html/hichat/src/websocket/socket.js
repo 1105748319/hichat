cd5646eb020ff6d603f9af728e6c37f8f17dd028
yun.liu <yun.liu@hualongdata.com>
2019-09-06 15:46:44 +0800

    修改websocket的BUG

--- html/hichat/src/websocket/socket.js
+++ html/hichat/src/websocket/socket.js
@@ -1,9 +1,12 @@
 var socket = function ($f7, token) {
-  var url = "ws://127.0.0.1:8082/";
+ // var ips = returnCitySN["cip"];
+  var url = "ws://60.165.143.216:8079/";
   var ws = null;
   function initWs($f7, token) {
     if (!!window['WebSocket']) {
-      ws = new WebSocket(url + token);
+
+      //ws = new WebSocket("ws://" + url?token + "/webSocketServer");
+      ws = new WebSocket(url+token);
     } else {
       $f7.alert("不支持websocket通信，请联系管理员！", "hichat");
     }
